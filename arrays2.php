aeb7a35785d8243d32d33ef8ca9079ba8050cc50
Bilja <biljanalj85@gmail.com>
2018-01-25 10:25:37 +0100

    Arrays exercises.

---
+++ arrays2.php
@@ -0,0 +1,24 @@
+<?php
+/**
+Write a PHP script to get the first element of the above array
+
+$color = array(4 => 'white', 6 => 'green', 11=> 'red');
+
+echo reset($color);
+ *
+ *  */
+
+
+/**
+Write a PHP script that inserts a new item in an array in any position
+ */
+
+$numbers = array(1, 2, 3, 4, 5);
+$newNumber = '$';
+
+array_push($numbers,$newNumber);
+
+print_r ($numbers);
+
+
+
